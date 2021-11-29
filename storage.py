03b7b093bad54851d3e715b5e46236944f63421e
Oleg Sokolov <klaus888@yandex.ru>
2021-11-29 19:55:12 +0300

    1

--- storage.py
+++ storage.py
@@ -8,37 +8,42 @@ def key_vals():
     parser.add_argument("--val", help="input value")
     args = parser.parse_args()
 
-    storage_dict = {}
+    storage_dict = read_from_file()
 
     key = str(args.key)
     val = str(args.val)
 
-    read_from_file()
+# val присваивается в словаре и записывается. А не должен.
+# если  вал нет, то поиск по ключам, если нет то вернуть Ноне
+# Если вал есть, то проверить на наличии ключа и добавить к значентю, если нет ключа то сделать новую пару ключ-значение
+
 
     if val == None:
         print(storage_dict.values(key))
 
-    elif key not in storage_dict:
-        storage_dict[key] = val
+    elif key in storage_dict:
+        storage_dict[key] += str(val)
         write_to_file(storage_dict)
     else:
-        storage_dict[key] = storage_dict[key] + val
-
+        storage_dict[key] = str(val)
         write_to_file(storage_dict)
-
 
 
 def write_to_file (storage: dict):
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'a') as f:
+    with open(storage_path, 'w') as f:
         f.write(str(storage))
         f.close()
 
-def read_from_file():
+def read_from_file() -> object:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'w') as f:
-        f.readable()
-        f.close()
-
+    with open(storage_path, 'r') as f:
+        x = f.read()
+        # if x == None: Тут надо правльно прочитать файл
+        #     return {}
+        # f.close()
+    return dict(x)
 
 key_vals()
+
+# Отправляет в файл, но перезаписывает
