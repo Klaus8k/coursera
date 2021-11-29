141493d06f70952652d8470e54f96e26cbdd2f3f
Oleg S home <tipkor@mail.ru>
2021-11-30 01:05:18 +0300

    555555555

--- storage.py
+++ storage.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import tempfile
+import json
+
 
 def key_vals():
     parser = argparse.ArgumentParser()
@@ -8,18 +10,37 @@ def key_vals():
     parser.add_argument("--val", help="input value")
     args = parser.parse_args()
 
-    storage_dict = read_from_file()
-    if len(storage_dict) != 0:
-        key = str(args.key)
-        val = str(args.val)
-        storage_dict = {key:[val,]}
-        write_to_file(storage_dict)
+
+    key = str(args.key)
+    val = str(args.val)
+
+    print(key,val)
+
+
+    if read_from_file():
+        dic = read_from_file()
+        if val:
+            dic[key] = dic[key] + str(val)
+            write_to_file(dic)
+            print(dic)
+
+        elif str(key) in dic:
+            print(dic[key])
+
+        else:
+            print('None')
+
+
+    else:
+        dic = {key: val}
+        write_to_file(dic)
 
 
 
-# val присваивается в словаре и записывается. А не должен.
-# если  вал нет, то поиск по ключам, если нет то вернуть Ноне
-# Если вал есть, то проверить на наличии ключа и добавить к значентю, если нет ключа то сделать новую пару ключ-значение
+
+
+
+
 
 
 
@@ -27,19 +48,18 @@ def key_vals():
 def write_to_file (storage: dict):
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
     with open(storage_path, 'w') as f:
-        f.write(str(storage))
-        f.close()
-#
+        json.dump(storage, f)
+
+
 def read_from_file() -> object:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'r+') as f:
-        lines = f.readline()
-        dic = {}
-        for line in lines:  # Проходимся по каждой строчке
-            key, value = line.split(': ')  # Разделяем каждую строку по двоеточии(в key будет - пицца, в value - 01)
-            dic.update({key: value}
+    with open(storage_path, 'r') as f:
+        if f.read():
+            json_str = json.load(f)
+            return json_str
+        return None
 
-    return dict(dic)
+
 
 key_vals()
 
