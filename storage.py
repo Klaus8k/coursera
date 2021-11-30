958cee5a99fa9dce3c2f2c0402ef49f62cf79a86
Oleg S home <tipkor@mail.ru>
2021-11-30 08:31:36 +0300

    555555555

--- storage.py
+++ storage.py
@@ -12,53 +12,47 @@ def key_vals():
 
 
     key = str(args.key)
-    val = str(args.val)
+    val = [args.val]
 
     print(key,val)
 
+    dic = read_from_file()
 
-    if read_from_file():
-        dic = read_from_file()
-        if val:
-            dic[key] = dic[key] + str(val)
+    if val:
+            dic.update([key, val.append(val)])
             write_to_file(dic)
             print(dic)
 
         elif str(key) in dic:
-            print(dic[key])
+            print(dic.values(key))
 
         else:
             print('None')
 
-
     else:
-        dic = {key: val}
+        print(1)
+        dic = dict.setdefault(key,[val])
         write_to_file(dic)
-
-
-
-
-
-
-
-
 
 
 
 def write_to_file (storage: dict):
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
     with open(storage_path, 'w') as f:
+        print(2222)
         json.dump(storage, f)
+
 
 
 def read_from_file() -> object:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'r') as f:
-        if f.read():
+    with open(storage_path, 'a') as f:
+        try:
             json_str = json.load(f)
-            return json_str
-        return None
-
+            return dict(json_str)
+        except:
+            print(1111111111111)
+            return None
 
 
 key_vals()
