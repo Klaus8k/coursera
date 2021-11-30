768f4e12a247f36fd6d9358f84c5bc928bf54886
Oleg Sokolov <klaus888@yandex.ru>
2021-11-30 11:49:38 +0300

    1

--- storage.py
+++ storage.py
@@ -3,58 +3,89 @@ import os
 import tempfile
 import json
 
-
-def key_vals():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--key", help="input key")
-    parser.add_argument("--val", help="input value")
-    args = parser.parse_args()
+dic = {}
 
 
-    key = str(args.key)
-    val = [args.val]
+while True:
+    key = input('key')
+    val = input('val')
 
-    print(key,val)
-
-    dic = read_from_file()
-
-    if val:
-            dic.update([key, val.append(val)])
-            write_to_file(dic)
-            print(dic)
-
-        elif str(key) in dic:
-            print(dic.values(key))
-
+    if val == '':
+        if  key in dic.keys():
+            print(dic[key])
         else:
-            print('None')
+            print(None)
 
     else:
-        print(1)
-        dic = dict.setdefault(key,[val])
-        write_to_file(dic)
+        if key in dic.keys():
+            old_val = dic[key]
+            old_val.append(val)
+            dic[key] = old_val
+
+        else:
+            dic[key] = [val]
 
 
 
-def write_to_file (storage: dict):
-    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'w') as f:
-        print(2222)
-        json.dump(storage, f)
+
+    print(dic)
 
 
 
-def read_from_file() -> object:
-    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'a') as f:
-        try:
-            json_str = json.load(f)
-            return dict(json_str)
-        except:
-            print(1111111111111)
-            return None
 
 
-key_vals()
+#
+#
+# def key_vals():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--key", help="input key")
+#     parser.add_argument("--val", help="input value")
+#     args = parser.parse_args()
+#
+#
+#     key = str(args.key)
+#     val = [args.val]
+#
+#     print(key,val)
+#
+#     dic = read_from_file()
+#
+#     if val:
+#             dic.update([key, val.append(val)])
+#             write_to_file(dic)
+#             print(dic)
+#
+#         elif str(key) in dic:
+#             print(dic.values(key))
+#
+#         else:
+#             print('None')
+#
+#     else:
+#         print(1)
+#         dic = dict.setdefault(key,[val])
+#         write_to_file(dic)
+#
+#
+#
+# def write_to_file (storage: dict):
+#     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+#     with open(storage_path, 'w') as f:
+#         print(2222)
+#         json.dump(storage, f)
+#
+#
+#
+# def read_from_file() -> object:
+#     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+#     with open(storage_path, 'a') as f:
+#         try:
+#             json_str = json.load(f)
+#             return dict(json_str)
+#         except:
+#             print(1111111111111)
+#             return None
+#
+#
+# key_vals()
 
-# Отправляет в файл, но перезаписывает
