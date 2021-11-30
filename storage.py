a21231f4c91f7d51e0f678aaef852fb047f6e601
Oleg Sokolov <klaus888@yandex.ru>
2021-11-30 12:26:38 +0300

    Не добавляет в словарь в файле следующие значения

--- storage.py
+++ storage.py
@@ -3,16 +3,22 @@ import os
 import tempfile
 import json
 
-dic = {}
 
 
-while True:
-    key = input('key')
-    val = input('val')
+def dic_form():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", help="input key")
+    parser.add_argument("--val", help="input value")
+    args = parser.parse_args()
 
-    if val == '':
+    key = args.key
+    val = args.val
+
+    dic = read_from_file()
+
+    if val == None:
         if  key in dic.keys():
-            print(dic[key])
+            print(', '.join(dic[key]))
         else:
             print(None)
 
@@ -21,71 +27,27 @@ while True:
             old_val = dic[key]
             old_val.append(val)
             dic[key] = old_val
+            write_to_file(dic)
 
         else:
             dic[key] = [val]
+            write_to_file(dic)
 
 
+def write_to_file (storage: dict):
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    with open(storage_path, 'w') as f:
+        json.dump(storage, f)
 
+def read_from_file() -> object:
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    with open(storage_path, 'w') as f:
+        try:
+            json_str = json.load(f)
+            return dict(json_str)
+        except:
+            json_str = {}
+            return json_str
 
-    print(dic)
-
-
-
-
-
-#
-#
-# def key_vals():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--key", help="input key")
-#     parser.add_argument("--val", help="input value")
-#     args = parser.parse_args()
-#
-#
-#     key = str(args.key)
-#     val = [args.val]
-#
-#     print(key,val)
-#
-#     dic = read_from_file()
-#
-#     if val:
-#             dic.update([key, val.append(val)])
-#             write_to_file(dic)
-#             print(dic)
-#
-#         elif str(key) in dic:
-#             print(dic.values(key))
-#
-#         else:
-#             print('None')
-#
-#     else:
-#         print(1)
-#         dic = dict.setdefault(key,[val])
-#         write_to_file(dic)
-#
-#
-#
-# def write_to_file (storage: dict):
-#     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-#     with open(storage_path, 'w') as f:
-#         print(2222)
-#         json.dump(storage, f)
-#
-#
-#
-# def read_from_file() -> object:
-#     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-#     with open(storage_path, 'a') as f:
-#         try:
-#             json_str = json.load(f)
-#             return dict(json_str)
-#         except:
-#             print(1111111111111)
-#             return None
-#
-#
-# key_vals()
+dic_form()
 
