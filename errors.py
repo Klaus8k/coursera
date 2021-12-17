ec312d91d8d595d28fcae81ee078bcefb534509e
Klaus8k <71668695+Klaus8k@users.noreply.github.com>
2021-12-17 20:00:56 +0300

    Update errors.py

--- errors.py
+++ errors.py
@@ -18,30 +18,63 @@ n = input()
 
 dic_er = {}
 for i in range(int(n)):
-
     x = input().split()
-    if len(x) != 1:
+    if ':' in x:
+        child = x[0]
+        par = x[2:]
+        if child in dic_er.keys():
+            dic_er[child]+=par
+        else:
+            dic_er.update({x[0]:x[2:]})
 
-        try:
-            dic_er.update({x[0]:x[2]})
-        except:
-            dic_er[x[0]].add(x[2])
     else:
-        x = {x[0]: None}
-        dic_er.update(x)
+        dic_er.update({x[0]:[]})
+    print(dic_er)
+
+
+
+
+
+    # if len(x) != 1:
+    #
+    #     if x[0] in dic_er.keys():
+    #         dic_er[x[0]]+=x[2:]
+    #     else:
+    #         dic_er.update({x[0]:[]})
+    # else:
+    #     dic_er.update({x[0]:[]})
+    # print(dic_er)
 
 # print(dic_er)
 
 m = input()
 out_ls = []
-
 for _ in range(int(m)):
     out_ls.append(input())
 
-for item in out_ls:
-    x = out_ls.pop()
-    if dic_er[x] in out_ls:
-        print(x)
+out_ls.reverse()
+
+def perent(child, par):
+    print('par', par)
+    if par in dic_er[child]:
+        print(child)
+        return '1'
+    else:
+        for i in dic_er[child]:
+            perent(i,par)
+
+
+    return False
+
+for children in out_ls:
+    for parent in out_ls[out_ls.index(children) + 1:]:
+
+        if perent(children,parent)  == '1':
+            print(children,'!')
+            break
+
+
+
 
 
 
