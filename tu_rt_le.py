1a7f58d6470d6c2c9f25f1cd4ae43ab5d71ff650
Oleg Sokolov <klaus888@yandex.ru>
2022-02-02 19:02:42 +0300

    turtle

--- tu_rt_le.py
+++ tu_rt_le.py
@@ -1,28 +1,62 @@
 from turtle import *
 import random
+import tkinter as tk
+# https://question-it.com/questions/768611/kak-ja-mogu-sohranit-holst-turtle-kak-izobrazhenie-png-ili-jpg-v-python-3
 
-
-class Turt(Turtle):
-    screensize(2000, 2000)
+class Pencil(Turtle):
+    # screensize(1000, 1000)
     WAY_BOX = [1037, 345, 223, 75, 45, 835, 247, 335, 1045, 90, 1474, 90]
+    DIRECTION_WAY = ('r','l','r','l','r','r','l','r','h','h','h')
 
     @classmethod
     def rand_angle(self):
-        return random.randrange(-20, 20) / 10
+        return random.randrange(-1, 1) / 5
 
-    def left(self, angle: float) -> None:
-        angle = angle + Turt.rand_angle()
-        super(Turt, self).left(angle)
-        print(angle)
+    # def left(self, angle: float) -> None:
+    #     angle = angle + Pencil.rand_angle()
+    #     super(Pencil, self).left(angle)
+    #     print(angle)
+
+    def right(self, angle: float):
+        angle = angle + Pencil.rand_angle()
+        super(Pencil, self).right(angle)
+        # print(angle)
 
 
 def main():
-    x = Turt()
-    for i in Turt.WAY_BOX:
-        x.forward(i/10)
-        x.left(90)
+    x = Pencil()
+    x.speed(1000)
+    x.setheading(90)
+    start_pos = x.pos()
+    for i in Pencil.WAY_BOX:
+        x.forward(i)
+        if Pencil.DIRECTION_WAY[Pencil.WAY_BOX.index(i)] == 'r':
+            x.right(90)
+        elif Pencil.DIRECTION_WAY[Pencil.WAY_BOX.index(i)] == 'l':
+            x. right(270)
+        else:
+            x.right(45)
+    end_pos = x.pos()
+    # print(round(end_pos[0], 1))
+    del(x)
+
+    return start_pos, end_pos
+
+
+
+
 
 
 if __name__ == '__main__':
-    main()
+
+    start_pos, end_pos = main()
+    id = 0
+    while start_pos[0] != round(end_pos[0], 1) :
+        print(id)
+        id +=1
+
+        print('{} - start, {} - end'.format(start_pos, end_pos))
+
+        start_pos, end_pos = main()
+
     mainloop()
