868e78da90afce3f09cbbbbebce3a14ba2738117
Oleg S home <tipkor@mail.ru>
2022-02-05 18:45:01 +0300

    tcp client

--- clien_soket.py
+++ clien_soket.py
@@ -26,20 +26,20 @@ class Client():
 
     def get(self, name_and_metrics: str):
         data = f'get {name_and_metrics}\\n'
+        print(data)
         try:
             self.sock.send(data.encode('utf-8'))
-            print(self.sock.recv(1024).decode('utf-8'))
+            req = self.sock.recv(1024)
         except BaseException as ClientError:
-            print(data, ' - Fail')
             raise ClientError
+        print(req.decode('utf-8'))
+
+
 
 
 if __name__ == '__main__':
     x = Client('127.0.0.1', 20003)
-    x.put("eardrum.memory", 4200000)
-    print(x.__dict__)
-    x.put("eardrum.memory", 4200)
-
-    time.sleep(1)
-    x.get("eardrum.memory")
-    # x.get('*')
+    # x.put("eardrum.memory", 4200000)
+    # x.put("eardrum.memory", 4200)
+    # x.get("eardrum.memory")
+    x.get('*')
--- soket_server.py
+++ soket_server.py
@@ -1,16 +1,26 @@
 import socket
 
+answer_str = 'ok\\npalm.cpu 2.0 1150864247\\npalm.cpu 0.5 1150864248\\n\\n'
+
 with socket.socket() as sock:
     sock.bind(("",20003))
     sock.listen()
 
     while True:
         conn, addr = sock.accept()
+
+
         with conn:
-            data = conn.recv(1024)
-            if data:
-                print(data.decode('utf-8'))
-                conn.send(b'ok')
-            else:
-                conn.send(b'Erorr')
+            while True:
+                try:
+                    data = conn.recv(1024)
+                except OSError:
+                    continue
+                if data:
+                    print(data)
+                    print(data.decode('utf-8'))
+                    conn.send(answer_str.encode('utf-8'))
+                else:
+                    break
+
 
