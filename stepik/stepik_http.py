e44176dea206f2b2754e5369502dfdccaa439fc2
Oleg Sokolov <klaus888@yandex.ru>
2022-02-15 19:13:56 +0300

    22

--- stepik/stepik_http.py
+++ stepik/stepik_http.py
@@ -1,41 +1,36 @@
 import requests
 import re
 
-url = 'http://pastebin.com/raw/7543p0ns'
-# url = input()
+# url = 'http://pastebin.com/raw/7543p0ns'
+url = input()
 
-regul_pattern = r'\w+[\.]\w+[\.\w+]+\b'
+regul_pattern = r'href=.{,8}[\w-]+[\.]\w+[\.\w+]+\/'
 
 def search_ref(url: str):
     r = requests.get(url)
     ref_list = r.text
     ref_list.encode('utf-8')
     return ref_list
-    # with open('1.txt', 'w+') as file:
-    #
-    #     file.write(ref_list)
-    #     r_file = file.read()
-    #     print(r_file)
-    #
-    # return r_file
 
-
-
-
-# with open('1.txt', 'r') as url:
-#     r = url.read()
-#
-#     result = re.findall(regul_pattern, r)
-#     result.sort()
-#     for i in result:
-#         print(i)
 
 responce_file = search_ref(url)
-print(responce_file)
+# print(responce_file)
 result = re.findall(regul_pattern, responce_file)
-result.sort()
+
+result_list = []
+site =''
+
 for i in result:
-    print(i)
+    site = re.search(r'[\w\-]+\..+', i)
+    result_list.append(site.group().rstrip('/'))
+
+set_result = set(result_list)
+list_to_print = list(set_result)
+
+list_to_print.sort()
+print('\n'.join(list_to_print))
+
+
 
 
 
