f7c39053e0a8902017f7bfc5e67a226b3a4ed35d
Oleg S home <tipkor@mail.ru>
2022-02-17 00:02:16 +0300

    TCP async server + test. Coursera

--- stepik/pars_url.py
+++ stepik/pars_url.py
@@ -1,40 +1,49 @@
 import requests
 import re
 import urllib.parse as urlpars
-import html.parser
+from html.parser import HTMLParser
 
-# url = 'http://pastebin.com/raw/7543p0ns'
-url = input()
+url = 'http://pastebin.com/raw/7543p0ns'
+# url = input()
 
-# regul_pattern = r'href=.{,8}[\w-]+[\.]\w+[\.\w+]+\/'
+class MyParce(HTMLParser):
+    def __init__(self):
+        super(MyParce, self).__init__()
+        self.reset()
+        self.result = None
+
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            self.result = attrs
+
+
 
 def search_ref(url: str):
     r = requests.get(url)
     ref_list = r.text
     ref_list.encode('utf-8')
     return ref_list
-https://docs.python.org/3.9/library/html.parser.html
 
 responce_file = search_ref(url)
 result_list = []
 
+parc = MyParce()
 for i in responce_file.splitlines():
-    list_ref = re.search(r'\<a href=(.+)\b', i)
-    if list_ref:
-        x = (urlpars.urlsplit(list_ref.group(1)[1:]))
-        result_list.append(re.sub(r'[\"\'\:].+','',x.netloc))
+    parc.result = None
+    parc.feed(i)
+    if parc.result and 'href' in parc.result[0]:
+        x = parc.result[0][1]
+        result_list.append(x)
 
-
-# result = re.findall(regul_pattern, responce_file)
-
-site =''
-
-# for i in result:
-#     site = re.search(r'[\w\-]+\..+', i)
-#     result_list.append(site.group().rstrip('/'))
-
-set_result = set(result_list)
-list_to_print = list(set_result)
-
-list_to_print.sort()
-print('\n'.join(list_to_print))
+for i in result_list:
+    x = re.sub(r'([\w]+://)([\w\.-]+)/',r'\2',i)
+    print(x)
+# site =''
+#
+#
+# set_result = set(result_list)
+# list_to_print = list(set_result)
+#
+# list_to_print.sort()
+# print('\n'.join(list_to_print))
