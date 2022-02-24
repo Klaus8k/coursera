87976725d5a3fab66f144858edbcfa6e9d6f3c89
Oleg Sokolov <klaus888@yandex.ru>
2022-02-24 17:27:15 +0300

    json stepik

--- stepik/artsy.py
+++ stepik/artsy.py
@@ -15,8 +15,38 @@ j = json.loads(r.text)
 token = j['token']
 headers = {'X-Xapp-Token' : token}
 
-artists_code = '4d8b92b34eb68a1b2c0003f4'
-art_url = 'https://api.artsy.net/api/artists/' + artists_code
-r = requests.get(art_url, headers=headers)
-j = json.loads(r.text)
-print(j)
+art_dict = {}
+
+with open('1.txt','r') as f:
+    artists_code = []
+    artists_code = f.read().split()
+# artists_code = ['4d8b92b34eb68a1b2c0003f4','537def3c139b21353f0006a6','4e2ed576477cc70001006f99']
+
+def request_art(art_code):
+    for i in art_code:
+        art_url = 'https://api.artsy.net/api/artists/' + i
+        r = requests.get(art_url, headers=headers)
+        r.encoding = 'utf-8'
+        j = json.loads(r.text)
+
+        # print(j.keys()) # dict_keys(['id', 'slug', 'created_at', 'updated_at', 'name', 'sortable_name', 'gender', 'biography',
+                        # 'birthday', 'deathday', 'hometown', 'location', 'nationality', 'target_supply', 'image_versions', '_links'])
+        art_dict[j['sortable_name']] = j['birthday']
+    return art_dict
+
+# request_art(artists_code)
+# test_list = [['Andy Warhol', '1928'], ['Mary Abbott', '1921'],['Hamra Abbas', '1976'],['Aamra RRRRR', '1976']]
+
+def sort_art(dict_a:dict):
+    x = sorted(dict_a.items(), key=lambda x: (x[1],x[0]))
+    return x
+
+y = request_art(artists_code)
+
+x = sort_art(y)
+
+
+with open('2.txt','w', encoding='utf-8') as f:
+    for i, val in enumerate(x):
+        print(val)
+        f.write(val[0] + '\n')
