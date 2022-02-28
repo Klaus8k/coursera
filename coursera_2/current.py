30c2d690fd260a03dbcbeaf9747e107fbd7b9eab
Oleg Sokolov <klaus888@yandex.ru>
2022-02-28 18:57:50 +0300

    coursera_2

--- coursera_2/current.py
+++ coursera_2/current.py
@@ -1,13 +1,9 @@
-def fibonacci(n):
-    assert n >= 0
-    F = [0, 1] + [0]*n
 
-    for i in range(2, n+1):
-        print(F)
-        F[i] = F[i-1] + F[i-2]
-    assert F[n] >= 0 and F[n] == F[n-1] + F[n-2]
-    return F[n]
+def main():
+    pass
+
 
 
 if __name__ == '__main__':
-    print(fibonacci(10))
+    main()
+
