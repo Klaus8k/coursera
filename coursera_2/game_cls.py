8f7bafdc8d1c560b2ff47a20d1243d96a42cf5c9
Oleg Sokolov <klaus888@yandex.ru>
2022-03-11 18:24:18 +0300

    screen with classes

--- coursera_2/game_cls.py
+++ coursera_2/game_cls.py
@@ -7,6 +7,7 @@ import math
 
 SCREEN_DIM = (800, 600)
 
+# Вектор от начала координат
 class Vec2d():
     def __init__(self, pos: set):
         self.x = pos[0]
@@ -14,19 +15,19 @@ class Vec2d():
 
     def __sub__(self, x, y):
         """"возвращает разность двух векторов"""
-        return x[0] - y[0], x[1] - y[1]
+        return self.x[0] - self.y[0], self.x[1] - self.y[1]
 
     def __add__(self, x, y):
         """возвращает сумму двух векторов"""
-        return x[0] + y[0], x[1] + y[1]
+        return self.x[0] + y[0], self.x[1] + y[1]
 
     def len(self, x):
         """возвращает длину вектора"""
-        return math.sqrt(x[0] * x[0] + x[1] * x[1])
-
-    def __mul__(self, v, k):
+        return math.sqrt(self.x[0] * self.x[0] + self.x[1] * self.x[1])
+    # Что то хуйня какая то
+    def __mul__(self, k):
         """возвращает произведение вектора на число"""
-        return v[0] * k, v[1] * k
+        return self[0] * k, self[1] * k
 
     def int_pair(self, x, y):
         """возвращает пару координат, определяющих вектор (координаты точки конца вектора),
@@ -35,10 +36,25 @@ class Vec2d():
 
 
 class Polyline():
-    def __init__(self, points=()):
-        self.points = points
+    def __init__(self, points_obj):
+        self.points_obj = points_obj
+        self.points_obj = []
+        self.speeds = []
 
-    def add_point(self, ):
+    def add_point(self, vec: object, speed):
+        self.points_obj.append(vec)
+        self.speeds.append(speed)
+
+    def set_points(self):
+        """функция перерасчета координат опорных точек"""
+        points = list(map(lambda x: x, self.points_obj))
+        for p in range(len(self.points_obj)):
+            points[p] = (points[p][0] + self.speeds[p][0], points[p][1] + self.speeds[p][1])
+            if points[p][0] > SCREEN_DIM[0] or points[p][0] < 0:
+                self.speeds[p] = (- self.speeds[p][0], self.speeds[p][1])
+            if points[p][1] > SCREEN_DIM[1] or points[p][1] < 0:
+                self.speeds[p] = (self.speeds[p][0], -self.speeds[p][1])
+
 
     def draw_points(points, style="points", width=3, color=(255, 255, 255)):
         """функция отрисовки точек на экране"""
@@ -53,14 +69,6 @@ class Polyline():
                 pygame.draw.circle(gameDisplay, color,
                                    (int(p[0]), int(p[1])), width)
 
-    def set_points(self, points, speeds):
-        """функция перерасчета координат опорных точек"""
-        for p in range(len(points)):
-            points[p] = add(points[p], speeds[p])
-            if points[p][0] > SCREEN_DIM[0] or points[p][0] < 0:
-                speeds[p] = (- speeds[p][0], speeds[p][1])
-            if points[p][1] > SCREEN_DIM[1] or points[p][1] < 0:
-                speeds[p] = (speeds[p][0], -speeds[p][1])
 
 
 
