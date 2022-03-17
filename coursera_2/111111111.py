20363a24fb86fd5a63bc23df98e6e1bfc84c71c7
Oleg Sokolov <klaus888@yandex.ru>
2022-03-17 17:36:31 +0300

    screen with classes

--- coursera_2/111111111.py
+++ coursera_2/111111111.py
@@ -1,82 +1,10 @@
-from abc import ABC, abstractmethod
+ma = [[0 for i in range(10)] for _ in range(10)]
+ma[5][7] = 4
+ma[5][5] = -4
+x = []
+for i in range(len(ma)):
+    x=[]
+    for j in range(len(ma)):
+        x.append(ma[i][j])
 
-
-class Delivery():
-    """
-    Класс описывающий объект, содержащий информацию о доставке для заказа
-    """
-
-    # для упрощения кода, пусть будет только один атрибут
-    # time_delivery - время доставки, не будем добавлять id
-    # заказа и другие "нужные" данные
-    def __init__(self, time_delivery=None):
-        self.time_delivery = time_delivery or 10
-
-    def get_time_delivery(self):
-        """возвращает время доставки по заказу"""
-        return self.time_delivery
-
-
-# На время доставки могут влиять разные факторы - проведение
-# специальных акций, выбор специальных условий при заказе,
-# например: экспресс доставка или оплата наличными. В начальный
-# момент времени (при написании кода) нельзя учесть все возможные
-#  действия, которые возможно будут добавлены.
-class AbstractDeliveryAction(ABC, Delivery):
-    """Абстрактный для всех действий класс"""
-
-    def __init__(self, base):
-        self.base = base
-        self.actions = []
-
-    @abstractmethod
-    def get_time_delivery(self):
-        pass
-
-
-class SpecialOffer(AbstractDeliveryAction):
-    """Специальное предложение - сокращает срок доставки на 3 дня"""
-
-    def get_time_delivery(self):
-        return self.base.get_time_delivery() - 3
-
-
-class ExpressDelivery(AbstractDeliveryAction):
-    """Экспресс доставка сокращает срок доставки на 10 дней"""
-
-    def get_time_delivery(self):
-        return self.base.get_time_delivery() - 5
-
-
-class CardPayment(AbstractDeliveryAction):
-    """Оплата картой увеличивает срок доставки на 1 день"""
-
-    def get_time_delivery(self):
-        return self.base.get_time_delivery() + 1
-
-
-# обратите внимание, что мы не изменяем непосредственно self.base
-# а возвращаем его изменененную копию, так происходит магия декораторов
-# создадим заказ
-order = Delivery()
-
-print(order.get_time_delivery())
-# будем применять действия к заказу, в зависимости от желания
-# заказчика и действующих акций
-order_expr = ExpressDelivery(order)
-print(order_expr.__dict__)
-print(order_expr.get_time_delivery())
-order_expr_spec = SpecialOffer(order_expr)
-print(order_expr_spec.get_time_delivery())
-order_total = CardPayment(order_expr_spec)
-print(order_total.get_time_delivery())
-
-# уберем экспресс доставку, если вдруг заказчик передумал
-order_expr_spec.base = order_expr_spec.base.base
-print(order_expr_spec.__dict__)
-print(order_total.get_time_delivery())
-# вывод справа в окне терминала -->
-# такая реализация позволяет без проблем добавлять новые действия
-# и их можно без проблем применять к уже существующим заказам
-
-# Это учебный пример, поэтому простой и немного натянут за уши :)
+    print(x)
--- coursera_2/test.py
+++ coursera_2/test.py
@@ -1,136 +1,47 @@
 from abc import ABC, abstractmethod
 
+class System:
+    def __init__(self):
+        self.map = self.grid = [[0 for i in range(30)] for _ in range(20)]
+        self.map[5][7] = 1  # Источники света
+        self.map[5][2] = -1  # Стены
 
-class AbstractEffect(ABC, Hero):
-    def __init__(self, base):
-        self.base = base
+    def get_lightening(self, light_mapper):
+        self.lightmap = light_mapper.lighten(self.map)
 
-    @abstractmethod
-    def get_positive_effects(self):
-        return self.base.get_positive_effects()
+class MappingAdapter:
+    def __init__(self, adaptee):
+        self.adaptee = adaptee
 
-    @abstractmethod
-    def get_negative_effects(self):
-        return self.base.get_negative_effects
-
-    @abstractmethod
-    def get_stats(self):
-        return self.base.get_stats()
-
-
-class AbstractPositive(AbstractEffect):
-    def __init__(self, base):
-        self.base = base
-
-    @abstractmethod
-    def get_positive_effects(self):
-        return self.base.get_positive_effects()
-
-    def get_negative_effects(self):
-        return self.base.get_negative_effects()
-
-    @abstractmethod
-    def get_stats(self):
-        return self.base.get_stats()
+    def lighten(self, grid):
+        self.adaptee.set_dim(grid)
+        for i in len(grid):
+            for j in len(grid):
+                if
 
 
-class AbstractNegative(AbstractEffect):
-    def __init__(self, base):
-        self.base = base
 
-    def get_positive_effects(self):
-        return self.base.get_positive_effects()
+class Light:
+    def __init__(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+        self.lights = []
+        self.obstacles = []
 
-    @abstractmethod
-    def get_negative_effects(self):
-        return self.base.get_negative_effects()
+    def set_dim(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
 
-    @abstractmethod
-    def get_stats(self):
-        return self.base.get_stats()
+    def set_lights(self, lights):
+        self.lights = lights
+        self.generate_lights()
 
+    def set_obstacles(self, obstacles):
+        self.obstacles = obstacles
+        self.generate_lights()
 
-class Berserk(AbstractPositive):
-    def __init__(self, base):
-        self.base = base
+    def generate_lights(self):
+        return self.grid.copy()
 
-    def get_stats(self):
-        stats = self.base.get_stats()
-        stats['Strength'] += 7
-        stats['Endurance'] += 7
-        stats['Agility'] += 7
-        stats['Luck'] += 7
-        for i in ('Charisma', 'Perception', 'Intelligence'):
-            stats[i] -= 3
-        stats['HP'] += 50
-        return stats
-
-    def get_positive_effects(self):
-        positive_effects = self.base.get_positive_effects()
-        positive_effects.append('Berserk')
-        return positive_effects
-
-
-class Blessing(AbstractPositive):
-    def __init__(self, base):
-        self.base = base
-
-    def get_stats(self):
-        stats = self.base.get_stats()
-        for key in stats.keys():
-            if len(key) > 2:
-                stats[key] += 2
-        return stats
-
-    def get_positive_effects(self):
-        positive_effects = self.base.get_positive_effects()
-        positive_effects.append('Blessing')
-        return positive_effects
-
-
-class Weakness(AbstractNegative):
-    def __init__(self, base):
-        self.base = base
-
-    def get_stats(self):
-        stats = self.base.get_stats()
-        for i in ('Strength', 'Endurance', 'Agility'):
-            stats[i] -= 4
-        return stats
-
-    def get_negative_effects(self):
-        negative_effects = self.base.get_negative_effects()
-        negative_effects.append('Weakness')
-        return negative_effects
-
-
-class EvilEye(AbstractNegative):
-    def __init__(self, base):
-        self.base = base
-
-    def get_stats(self):
-        stats = self.base.get_stats()
-        stats['Luck'] -= 10
-        return stats
-
-    def get_negative_effects(self):
-        negative_effects = self.base.get_negative_effects()
-        negative_effects.append('EvilEye')
-        return negative_effects
-
-
-class Curse(AbstractNegative):
-    def __init__(self, base):
-        self.base = base
-
-    def get_stats(self):
-        stats = self.base.get_stats()
-        for key in stats.keys():
-            if len(key) > 2:
-                stats[key] -= 2
-        return stats
-
-    def get_negative_effects(self):
-        negative_effects = self.base.get_negative_effects()
-        negative_effects.append('Curse')
-        return negative_effects
+sys = System()
+sys.lightmap(MappingAdapter(Light))
