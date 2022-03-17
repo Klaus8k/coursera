734965abffdd29795c78ba4267e74f572c5e0cc9
Oleg Sokolov <klaus888@yandex.ru>
2022-03-17 18:43:08 +0300

    screen with classes

--- coursera_2/test.py
+++ coursera_2/test.py
@@ -16,14 +16,15 @@ class MappingAdapter:
     def lighten(self, grid):
         self.adaptee.set_dim(len(grid[0]), len(grid))
 
-        for i in range(len(grid)):
-            for j in range(len(grid)):
-                field = grid[i][j]
+        for i in range(len(self.adaptee.dim)):
+            for j in range(len(self.adaptee.dim)):
+                field = self.adaptee.dim[i][j]
                 if field == 1:
                     self.adaptee.set_lights(field)
                 elif field == -1:
                     self.adaptee.set_obstacles(field)
-        return self.adaptee.generate_lights()
+        l_map = self.adaptee.generate_lights()
+        return l_map
 
 
 class Light:
